c664a7ee34a772499c47bbc70971fd952045bb1c
Higor Campelo <higorxadrez@hotmail.com>
2021-09-06 18:53:35 -0300

    Project Created

---
+++ backend/src/test/java/com/devsupeior/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsupeior.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
